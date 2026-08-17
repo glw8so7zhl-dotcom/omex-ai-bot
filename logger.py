cd23711b05a3446ba0af7f49308ea3944e533931
glw8so7zhl-dotcom <glw8so7zhl@gmail.com>
2026-08-17 22:41:29 +0300

    Update logger.py
    Fix logger for Railway

--- logger.py
+++ logger.py
@@ -4,15 +4,19 @@ import logging
 def create_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(formatter)
-    fh = logging.FileHandler(f'log.txt')
-    fh.setLevel(logging.INFO)
-    fh.setFormatter(formatter)
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        ch.setFormatter(formatter)
+
+        logger.addHandler(ch)
+
     return logger
 
 
